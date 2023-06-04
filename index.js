01b152adc5457b89172d2ddfc2bbb75aad4411c0
root <root@Chris.localdomain>
2023-06-03 19:57:28 -0700

    Maybe no runescape

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-//http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=itemId
+//http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=10
+// https://pokeapi.co/api/v2/pokemon/ditto
 
-function searchGE(){
-    fetch("http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=10").then((response)=>response.json()).then((data)=>console.log(data))
-    const inputBox =document.getElementById("itemToInput")
-    const listContainer = document.getElementById("unordered")
-    if(inputBox.value==="")
-    {
-        // alert("You must write something")
-    }
-    else
-    {
+const price= async()=>{
+    fetch("http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=10").then(response=>response.json()).then(data=>console.log(data));
+}
         
-        let li=document.createElement("li")
-        li.innerHTML=inputBox.value;        
-        listContainer.appendChild(li);
-    }
-    inputBox.value="";
-    return listContainer;
-}
+    
 
-function getItemId(name){
-    const itemName =document.getElementById("itemToInput")
-    fetch(`https://oldschool.runescape.wiki/w/${name}`).then()
 
-}
+price();
