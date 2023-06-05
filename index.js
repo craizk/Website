aea7c3cf98365ec9d35fb112be104150c13d0166
root <root@Chris.localdomain>
2023-06-05 12:03:44 -0700

    Pokedex basically done

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const findPokemon= async()=>{
     .then(response=>response.json()).then(data=>data.sprites.front_default);
     const pokeType= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(response=>response.json()).then(data=>data.types[0].type.name);
+    const poke_url= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response=>response.json()).then(data=>data.species.url);
+    const poke_info=await fetch(poke_url)
+    .then(response=>response.json()).then(data=>data.flavor_text_entries[0].flavor_text);
+
     let poke_data=document.createElement("p");
     let Container = document.getElementById("pokeName")    
     let img=document.createElement("img")
@@ -15,12 +20,23 @@ const findPokemon= async()=>{
 
     poke_data.innerHTML=pokeName;
     Container.appendChild(poke_data);
+
     poke_data=document.createElement("p");
-    Container = document.getElementById("pokeTypes")
-    poke_data.innerHTML=pokeType 
-    Container.appendChild(poke_data)
-    Container=document.getElementById("sprite")
-    Container.appendChild(img)
+    Container = document.getElementById("pokeTypes");
+    poke_data.innerHTML=pokeType ;
+    Container.appendChild(poke_data);
+
+    Container=document.getElementById("sprite");
+    Container.appendChild(img);
+
+    poke_data=document.createElement("textarea");
+    
+    poke_data.rows="8";
+    poke_data.innerHTML=poke_info;
+    Container=document.getElementById("poke_info");
+    Container.appendChild(poke_data);
+    console.log("Poke_data",poke_data);
+
     
     return pokeType
 }
