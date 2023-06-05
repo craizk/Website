d5afff9f0be93c82a37c3339fdbab3c232d0314d
root <root@Chris.localdomain>
2023-06-05 13:04:41 -0700

    Added links to wiki

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const findPokemon= async()=>{
     
     const name =document.getElementById("itemToInput").value
-    
+    updateSideBar();
    const pokeName= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(response=>response.json()).then(data=>data.name);
     const pokePicture= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -12,7 +12,7 @@ const findPokemon= async()=>{
     .then(response=>response.json()).then(data=>data.species.url);
     const poke_info=await fetch(poke_url)
     .then(response=>response.json()).then(data=>data.flavor_text_entries[0].flavor_text);
-
+    
     let poke_data=document.createElement("p");
     let Container = document.getElementById("pokeName")    
     let img=document.createElement("img")
@@ -42,11 +42,24 @@ const findPokemon= async()=>{
 }
 
 const changeBackground=async()=>{
-
+    
     let img=document.createElement("img")
     img.src="imgs/grass_pokemon.jpg";
-    let Container = document.getElementById("body")
+    let Container = document.getElementById("background_image")
     Container.appendChild(img)
+
+
+}
+changeBackground();
+function updateSideBar(){
+let update=document.getElementById("Moves")
+let name=document.getElementById("itemToInput").value;
+
+update.href=`https://pokemon.fandom.com/wiki/${name}#Learnset`
+ update=document.getElementById("Evolutions")
+update.href=`https://pokemon.fandom.com/wiki/${name}#Evolution`
+ update=document.getElementById("Locations")
+update.href=`https://pokemon.fandom.com/wiki/${name}#Locations`
 
 
 }
