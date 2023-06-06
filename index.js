fbb85efc5c66953a1e524e2a6cd73d34ebc6a528
root <root@Chris.localdomain>
2023-06-06 10:37:25 -0700

    Figured out evolve once

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const findPokemon= async()=>{
         
     const name =document.getElementById("itemToInput").value
-    updateSideBar();
+    
    const pokeName= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(response=>response.json()).then(data=>data.name);
     const pokePicture= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -17,33 +17,37 @@ const findPokemon= async()=>{
     .then(response=>response.json()).then(data=>data.flavor_text_entries[0].flavor_text);
     
     let poke_data=document.createElement("p");
-    let Container = document.getElementById("pokeName")    
-    let img=document.createElement("img")
-    img.src=pokePicture;
-
-    poke_data.innerHTML=pokeName;
+    let Container = document.getElementById("pokeName")  
+     poke_data.innerHTML=pokeName;
     Container.appendChild(poke_data);
+
 
     poke_data=document.createElement("p");
     Container = document.getElementById("pokeTypes");
     poke_data.innerHTML="Type:"+pokeType ;
     Container.appendChild(poke_data);
 
+
+    let img=document.createElement("img")
+    img.src=pokePicture;
     Container=document.getElementById("sprite");
     Container.appendChild(img);
+
 
     img=document.createElement("img")
     img.src=pokePicture;
     Container=document.getElementById("sprite2");
     Container.appendChild(img);
 
+
     poke_data=document.createElement("textarea");
-    
     poke_data.rows="8";
     poke_data.innerHTML=poke_info;
     Container=document.getElementById("poke_info");
     Container.appendChild(poke_data);
-    console.log("Poke_data",poke_data);
+
+    updateSideBar();
+    
 
     
     
@@ -62,8 +66,9 @@ changeBackground();
 
 
 function updateSideBar(){
+    let name=document.getElementById("pokeName").getElementsByTagName('p')[0].innerHTML
 let update=document.getElementById("Moves")
-let name=document.getElementById("itemToInput").value;
+
 
 update.href=`https://pokemon.fandom.com/wiki/${name}#Learnset`
  update=document.getElementById("Evolutions")
@@ -77,7 +82,8 @@ update.href=`https://pokemon.fandom.com/wiki/${name}#Game_info`
 const showEvolveChain =async ()=>{
 
     // Gets the current pokemons next evolution
-    let name=document.getElementById("itemToInput").value
+    let name=document.getElementById("pokeName").getElementsByTagName('p')[0].innerHTML
+    
     const url1= await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(response=>response.json()).then(data=>data.species.url)
     const url2=await fetch(url1).then(response=>response.json()).then(data=>data.evolution_chain.url)
@@ -86,14 +92,52 @@ const showEvolveChain =async ()=>{
 
     const pokeName= await fetch(`https://pokeapi.co/api/v2/pokemon/${newEvolve}`)
     .then(response=>response.json()).then(data=>data.name);
-    console.log(pokeName)
-    // plugging in the new pokemons inforamtion
+    // console.log(pokeName)
 
-    let img =document.getElementById("sprite2")
+    // plugging in the new pokemons inforamtion
+    let img_url =await  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then(response=>response.json()).then(data=>data.sprites.front_default)
+
+    // getting evolved species info
+    const poke_url= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then(response=>response.json()).then(data=>data.species.url);
+    const poke_info=await fetch(poke_url).then(response=>response.json())
+    .then(data=>data.flavor_text_entries[0].flavor_text);
+
     
-    img.src= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+    // Create new evolved image
+    let img=document.createElement("img")
+    img.src= img_url;
+    
     Container=document.getElementById("sprite2");
-    Container.replaceChild(img,Container.childNodes[0])
+    Container.removeChild(Container.firstElementChild)   
+    Container.appendChild(img)
+    
+    img=document.createElement("img")
+    img.src= img_url;
+    Container=document.getElementById("sprite");
+    Container.removeChild(Container.firstElementChild)   
+    Container.appendChild(img)
+
+    // // Change Pokename
+    poke_data=document.createElement("p");
+    Container = document.getElementById("pokeName")
+    poke_data.innerHTML=pokeName;
+    Container.removeChild(Container.firstElementChild)
+    Container.appendChild(poke_data)
+    
+    // //Change Species info
+    Container=document.getElementById("poke_info");
+    Container.removeChild(Container.firstElementChild)
+    poke_data=document.createElement("textarea");
+    poke_data.rows="8";
+    poke_data.innerHTML=poke_info;
+    Container.appendChild(poke_data)
+
+    updateSideBar()
+
+    // 
+    
 }   
 
     
